20f079399ac1b5b09e1af7c54b5f1a8ab536761e
FrankOrtega <fortega@envios2224.com>
2021-05-18 09:04:51 -0400

    added api calls

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+    function success(position) {
+      const lat  = position.coords.latitude;
+      const lon = position.coords.longitude;
+      $.ajax({
+        type: "GET",
+        url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=167b57c505929b8da0a5d19017f6b443
+        &units=imperial`,
+        dataType: "json",
+        success: function(response) {
+          console.log(response);
+          $("#today").empty();
+          makeRow(response.name);
+          var currentTemp = Math.floor(response.main.temp);
+          var feelsTemp = Math.floor(response.main.feels_like)
+          var card = $("<div>").addClass("card");
+          var cardBody = $("<div>").addClass("card-body");
+          var title = $("<h1>").addClass("card-title").text(response.name);
+          var description = $("<h4>").addClass("card-text").text(response.weather[0].description)
+          var temp = $("<h4>").addClass("card-text").text("current tempature is: " + currentTemp +String.fromCharCode(176));
+          var feelsLike = $("<h4>").addClass("card-text").text("feels like " + feelsTemp +String.fromCharCode(176))
+          var wind = $("<h4>").addClass("card-text").text("current wind speed: " + response.wind.speed)
+       
+          $("#today").append(card);
+          card.append(cardBody);
+          cardBody.append(title,description,temp,feelsLike,wind)
+        }
+      })
+     
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+    if (!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  
+  
+    $("#search-btn").on("click", function(){
+      var searchValue = $("#search-value").val();
+  
+      $(".history").on("click","li",function() {
+        searchWeather($(this).text())
+      })
+  
+    
+      makeRow(searchValue);
+      searchWeather(searchValue)
+    });
+  
+    function makeRow(text) {
+      var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
+      $(".history").append(li)
+    }
+  
+    function searchWeather(searchValue) {
+      $.ajax({
+        type: "GET",
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=167b57c505929b8da0a5d19017f6b443
+        &units=imperial`,
+        dataType: "json",
+        success: function(response) {
+            console.log(response)
+          $("#today").empty();
+          var currentTemp = Math.floor(response.main.temp);
+          var feelsTemp = Math.floor(response.main.feels_like)
+          var card = $("<div>").addClass("card");
+          var cardBody = $("<div>").addClass("card-body");
+          var title = $("<h1>").addClass("card-title").text(response.name);
+          var description = $("<h4>").addClass("card-text").text(response.weather[0].description)
+          var temp = $("<h4>").addClass("card-text").text("current tempature is: " + currentTemp +String.fromCharCode(176));
+          var feelsLike = $("<h4>").addClass("card-text").text("feels like " + feelsTemp +String.fromCharCode(176))
+          var wind = $("<h4>").addClass("card-text").text("current wind speed: " + response.wind.speed)
+       
+          $("#today").append(card);
+          card.append(cardBody);
+          cardBody.append(title,description,temp,feelsLike,wind)
+        
+        }
+      
+      })
+    }
+  
+    
+  });
+  
